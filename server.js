508ab67d5b852372274e9fa56e2e9a4b7841f62f
TristanR99 <tristanrebello@gmail.com>
2019-09-05 16:46:03 -0400

    Implementing express.js

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 const http = require('http');
 
-const routes = require('./routes');
+const express = require('express');
 
-console.log(routes.someText);
+const app = express();
 
-const server = http.createServer(routes.handler);
+app.use((req, res, next) => {
+    
+});
+
+const server = http.createServer(app);
 
 server.listen(3000);
 
