756869d2c01df43509c91a7cd1defa3e5e3d41e7
TristanR99 <48964179+TristanR99@users.noreply.github.com>
2019-12-19 10:57:06 -0500

    Edits

--- server.js
+++ server.js
@@ -1,23 +1,19 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+const http = require("http");
+
+const express = require("express");
 
 const app = express();
 
-app.use(bodyParser.urlencoded({extended: false}));
-
-app.use('/add-product', (req, res, next) => {
-    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add product</button></form>');
+app.use((req, res, next) => {
+  console.log("In the middleware!");
+  next(); // Allows the request to continue to the next middleware in line
 });
 
-app.post('/product', (req, res, next) => {
-    console.log(req.body);
-    res.redirect('/');
+app.use((req, res, next) => {
+  console.log("In another middleware!");
+  res.send("<h1>Hello from Express!</h1>");
 });
 
-app.use('/', (req, res, next) => {
-    res.send('<h1>Hello from Express!</h1>');
-});
+const server = http.createServer(app);
 
-app.listen(3000);
-
-
+server.listen(3000);
